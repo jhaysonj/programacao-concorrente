37f473f87d0712323292d0c789a2a60d804c0f80
jhaysonj <jhaysonbj@dcc.ufrj.br>
2024-03-26 21:12:50 +0000

    atualizando atividade 1

--- atividade 1/atividade_5.c
+++ atividade 1/atividade_5.c
@@ -3,8 +3,8 @@
 #include <pthread.h>
 #include <stdbool.h>
 
-#define M_THREADS 6 //total de threads a serem criadas
-#define N_NUMEROS 25 //quantidade de números no meu vetor
+#define M_THREADS 100 //total de threads a serem criadas
+#define N_NUMEROS 50 //quantidade de números no meu vetor
 
 int vetor[N_NUMEROS]; // vetor a ser manipulado pelas 'M' threads
 int vetor_original[N_NUMEROS]; // vetor que mantem o mesmo valor para validarmos as operações feitas
@@ -36,7 +36,7 @@ void * eleva_ao_quadrado (void * arg){
 }
 
 
-
+// defini arbitrariamente que o vetor de teste seria iniciado de 1 até N_NUMEROS
 void inicializa_vetor(){
     printf("Vetor original:\n");
     // inicializa o vetor
@@ -52,7 +52,12 @@ void inicializa_vetor(){
 }
 
 bool valida_resultado(){
+    // lida com o caso de divisão por zero
     for(int i = 0; i<N_NUMEROS; i++){
+        if((vetor[i] == 0) && (vetor[i] == vetor_original[i])){
+            printf("Nenhum erro encontrado, vetor validado com sucesso!\n");
+            return true;
+        }
         if(vetor[i]/vetor_original[i] != vetor_original[i]){
             printf("ERRO %d² != %d\n", vetor_original[i], vetor[i]);
             return false;
@@ -69,13 +74,24 @@ int main(int argc, char *argv[]){
     pthread_t tid_sistema[M_THREADS]; // identificadores das threads no sistema 
 
     // atribuição de tarefas: define a quantidade de elementos do vetor que cada thread elevará ao quadrado
-    quantidade_de_tarefas = N_NUMEROS/M_THREADS;
-
+    
+    if(M_THREADS>N_NUMEROS){
+        quantidade_de_tarefas = 1;
+    }else{
+        quantidade_de_tarefas = N_NUMEROS/M_THREADS;
+    }
+    
     inicializa_vetor();
 
     printf("\n\n");
     t_Args *args; //receberá os argumentos para a thread
     for(int i=0; i<M_THREADS; i++){
+        
+        // lida com os casos em que falta há mais thread do que tarefas
+        if(i>=N_NUMEROS){
+            break;
+        }
+
         // Aloca e preenche argumentos para I-ésima thread
         args = malloc(sizeof(t_Args));
         
@@ -86,7 +102,6 @@ int main(int argc, char *argv[]){
         // lida com o caso em que a variavel N_NUMEROS não é múltiplo de M_THREADS, ou seja, alguma thread tem que fazer mais operações do que as outras
         // condição ? valor_se_verdadeiro : valor_se_falso;
         args->index_final = (i == M_THREADS - 1) ? N_NUMEROS :  args->index_inicial + quantidade_de_tarefas;
-        
         // Cria a thread
         if (pthread_create(&tid_sistema[i], NULL, eleva_ao_quadrado, (void*) args)) {
         printf("--ERRO: pthread_create()\n"); exit(-1);
@@ -97,6 +112,10 @@ int main(int argc, char *argv[]){
 
     // espera todas as M_THREADS terminarem
     for (int i=0; i<M_THREADS; i++) {
+        // lida com os casos em que falta há mais thread do que tarefas
+        if(i>=N_NUMEROS){
+            break;
+        }
         if (pthread_join(tid_sistema[i], NULL)) {
             printf("--ERRO: pthread_join() \n"); exit(-1); 
         } 
